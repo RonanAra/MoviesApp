0d76797fb6ee0c15e08b8635cd869dc891666b65
Ronan Fernandes <ronanlm10@hotmail.com>
2023-05-12 19:44:25 -0300

    feat: Config hilt

--- app/src/main/java/com/example/moviesapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/moviesapp/presentation/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.moviesapp.presentation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.moviesapp.R
+import com.example.moviesapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
