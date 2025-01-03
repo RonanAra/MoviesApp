778597797850063ec18abb92cadcb9aa70f215fd
Ronan Fernandes <ronanlm10@hotmail.com>
2025-01-03 19:50:39 -0300

    refactor: Moved repository to data package

--- app/src/main/java/com/example/moviesapp/di/RepositoryModule.kt
+++ app/src/main/java/com/example/moviesapp/di/RepositoryModule.kt
@@ -1,9 +1,9 @@
 package com.example.moviesapp.di
 
-import com.example.moviesapp.data.remote.datasource.MoviesDataSource
-import com.example.moviesapp.data.remote.datasource.MoviesDataSourceImpl
-import com.example.moviesapp.data.remote.repository.MoviesRepository
-import com.example.moviesapp.data.remote.repository.MoviesRepositoryImpl
+import com.example.moviesapp.data.datasource.MoviesDataSource
+import com.example.moviesapp.data.datasource.MoviesDataSourceImpl
+import com.example.moviesapp.data.repository.MoviesRepository
+import com.example.moviesapp.data.repository.MoviesRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
