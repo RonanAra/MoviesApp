7d26acbb2d57cd58857aa77d65e2bd764352a100
Ronan Fernandes <ronanlm10@hotmail.com>
2025-01-12 09:11:38 -0300

    fix: extract string to resources

--- app/src/main/java/com/example/moviesapp/utils/extensions/ViewExtensions.kt
+++ app/src/main/java/com/example/moviesapp/utils/extensions/ViewExtensions.kt
@@ -2,11 +2,12 @@ package com.example.moviesapp.utils.extensions
 
 import android.content.Context
 import android.widget.Toast
+import com.example.moviesapp.R
 
 fun Context.notImplFeature(msg: String? = null) {
     Toast.makeText(
         this,
-        msg ?: "Função não Implementada!",
+        msg ?: getString(R.string.not_impl_feature_message),
         Toast.LENGTH_SHORT
     ).show()
 }
